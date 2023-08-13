bcfb138d74f9013904150ca38977f5c8e4f6b15c
Stephen Davis <68449281+nativeit@users.noreply.github.com>
2023-08-13 03:37:08 -0400

    Create staging.sh

---
+++ scripts/staging.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+###########################################################
+# Staging
+###########################################################
+
+mkdir /tmp/nativeit
+cp ${PWD}/scripts/lib/0-lib-bash-utils.sh /tmp/nativeit/lib-bash-utils.sh
+cp ${PWD}/scripts/lib/0-lib-system-utils.sh /tmp/nativeit/lib-system-utils.sh
+cp ${PWD}/scripts/lib/0-lib-system-debian.sh /tmp/nativeit/lib-system-debian.sh
+cp ${PWD}/scripts/lib/0-setup-system.sh /tmp/nativeit/init.sh
+
+chmod +x /tmp/nativeit/*.sh
+
+echo "Type [ sh /tmp/init.sh ] into your prompt to start!"
